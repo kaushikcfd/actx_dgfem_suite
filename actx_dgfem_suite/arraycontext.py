3f966f543540ad1e151d3ba1018b24d55512bd88
Kaushik Kulkarni <kaushikcfd@gmail.com>
2026-01-30 11:06:23 -0800

    Apply distributive law to mass inv.

--- actx_dgfem_suite/arraycontext.py
+++ actx_dgfem_suite/arraycontext.py
@@ -1,4 +1,32 @@
+import loopy as lp
+import pytato as pt
 from arraycontext import PytatoPyOpenCLArrayContext
+
+
+def apply_distributive_law_to_mass_inverse(
+    expr: pt.DictOfNamedArrays,
+) -> pt.AbstractResultWithNamedArrays:
+    from pytato.transform.einsum_distributive_law import (
+        DoDistribute,
+        DoNotDistribute,
+        EinsumDistributiveLawDescriptor,
+        apply_distributive_property_to_einsums,
+    )
+
+    def how_to_distribute(expr: pt.Einsum) -> EinsumDistributiveLawDescriptor:
+        if pt.analysis.is_einsum_similar_to_subscript(expr, "e,ij,ej->ei"):
+            return DoDistribute(ioperand=2)
+        else:
+            return DoNotDistribute()
+
+    expr = pt.make_dict_of_named_arrays(
+        {
+            name: apply_distributive_property_to_einsums(subexpr, how_to_distribute)
+            for name, subexpr in expr._data.items()
+        }
+    )
+
+    return expr
 
 
 class DGFEMOptimizerArrayContext(PytatoPyOpenCLArrayContext):
@@ -10,10 +38,21 @@ class DGFEMOptimizerArrayContext(PytatoPyOpenCLArrayContext):
     See paper (TODO) for details.
     """
 
-    def transform_dag(self, dag):
+    def transform_dag(
+        self, dag: pt.AbstractResultWithNamedArrays
+    ) -> pt.AbstractResultWithNamedArrays:
+        if pt.analysis.get_num_nodes(dag) < 10:
+            return super().transform_dag(dag)
+
+        # pt.show_fancy_placeholder_data_flow(dag)
+
+        dag = apply_distributive_law_to_mass_inverse(dag)
+
         # TODO: Implement the transformations. here.
         return super().transform_dag(dag)
 
-    def transform_loopy_program(self, t_unit):
+    def transform_loopy_program(
+        self, t_unit: lp.TranslationUnit
+    ) -> lp.TranslationUnit:
         # TODO: Implement the transformations.
         return super().transform_loopy_program(t_unit)
