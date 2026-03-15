2499d48bbda2b6da88fbb9fbefdf025687600c55
Kaushik Kulkarni <kaushikcfd@gmail.com>
2026-03-14 17:14:07 -0700

    Use correct name of kennedy fusion routine.

--- actx_dgfem_suite/arraycontext/dgfem_optimizer.py
+++ actx_dgfem_suite/arraycontext/dgfem_optimizer.py
@@ -14,7 +14,7 @@ from actx_dgfem_suite.arraycontext.distribute_operands_of_mass_einsum import (
     apply_distributive_law_to_mass_inverse,
 )
 from actx_dgfem_suite.arraycontext.kennedy_loop_fusion import (
-    apply_kennedy_loop_fusion_for_einsum_tag,
+    apply_kennedy_loop_fusion_for_einsum_tags,
 )
 from actx_dgfem_suite.arraycontext.mass_inverse_fuser import fuse_mass_inverses
 from actx_dgfem_suite.arraycontext.materialization_policy import (
@@ -77,7 +77,7 @@ class DGFEMOptimizerArrayContext(PytatoPyOpenCLArrayContext):
         ):
             return self.comptime_actx.transform_loopy_program(t_unit)
 
-        t_unit = apply_kennedy_loop_fusion_for_einsum_tag(t_unit)
+        t_unit = apply_kennedy_loop_fusion_for_einsum_tags(t_unit)
         print(t_unit)
         print(f"Number of insns = {len(t_unit.default_entrypoint.instructions)}.")
 
