d949cc80e9c761ac78a3d36fef22c770dbf8192f
Kaushik Kulkarni <kaushikcfd@gmail.com>
2026-06-25 13:49:21 -0500

    Add device peak info for H200 NVL.

--- actx_dgfem_suite/consts.py
+++ actx_dgfem_suite/consts.py
@@ -1,5 +1,12 @@
 # Mapping from device name to peak flop rate in GFlOps/s
-DEV_TO_PEAK_F64_GFLOPS = {"NVIDIA TITAN V": 6144}
+DEV_TO_PEAK_F64_GFLOPS = {
+    "NVIDIA TITAN V": 6144,
+    # forcing vector only (for now) for loopy.
+    "NVIDIA H200 NVL": 30000,  # https://www.nvidia.com/en-in/data-center/h200/
+}
 
 # Mapping from device name to peak bandwidth in GB/s
-DEV_TO_PEAK_BW = {"NVIDIA TITAN V": 652.8}
+DEV_TO_PEAK_BW = {
+    "NVIDIA TITAN V": 652.8,
+    "NVIDIA H200 NVL": 4800,  # https://www.nvidia.com/en-in/data-center/h200/
+}
